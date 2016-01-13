c9e62c458cca18f1515515ec158e3980d138a994
Nick Landolfi <nclandolfi@gmail.com>
2016-01-13 14:20:16 -0800

    refactor input.go

--- command/input.go
+++ command/input.go
@@ -8,145 +8,143 @@ import (
 	"github.com/mitchellh/cli"
 )
 
-func yesNo(ui cli.Ui, text string) (b bool, err error) {
-	var i string
-	i, err = ui.Ask(text + " [y to confirm]:")
-	if err == nil {
-		b = (i == "y")
-	}
-	return
+// yesNo requests confirmation of something
+//
+// Use this for deciding what to do, like whether to request
+// additional information from a user or whether the user
+// intended to do something.
+func yesNo(ui cli.Ui, text string) (bool, error) {
+	i, err := ui.Ask(text + " [y to confirm]")
+	return (i == "y"), err
 }
 
+// stringInput requests textual input
+//
+// User this where you need to take a string value, or where
+// you would want to parse a string input yourself
 func stringInput(ui cli.Ui, text string) (string, error) {
 	return ui.Ask(text + " [string]:")
 }
 
-func boolInput(ui cli.Ui, text string) (b bool, err error) {
-	var input string
-
+// boolInput requests a boolean input
+//
+// Use this where you need to take a boolean value. If you are
+// looking for a confirmation prompt, however, use 'yesNo'
+func boolInput(ui cli.Ui, text string) (bool, error) {
 	for {
-		input, err = ui.Ask(text + " [boolean]:")
+		input, err := ui.Ask(text + " [boolean]:")
 		if err != nil {
-			return
+			return false, err
 		}
 
-		if input == "yes" {
-			b = true
-			return
-		}
+		switch input {
+		case "yes":
+			return true, err
+		case "no":
+			return false, err
+		default:
+			b, err := strconv.ParseBool(input)
+			if err == nil {
+				return b, nil
+			}
 
-		if input == "no" {
-			b = false
-			return
-		}
-
-		b, err = strconv.ParseBool(input)
-
-		if err != nil {
-			out := `
-Invalid input, please try again.
-
-Valid boolean expressions include: true, false, 0, 1 etc.
-		`
+			out := " Invalid input, please try again. Valid boolean expressions include: true, false, 0, 1 etc."
 			ui.Output(strings.TrimSpace(out))
-		} else {
-			return
 		}
 	}
 }
 
-func intInput(ui cli.Ui, text string) (i int, err error) {
-	var input string
-
+// intInput requests an integer input (signed)
+//
+// Use intInput if you need to retrieve an integer.
+func intInput(ui cli.Ui, text string) (int, error) {
 	for {
-		input, err = ui.Ask(text + " [integer]:")
+		input, err := ui.Ask(text + " [integer]:")
 		if err != nil {
-			return
+			return 0, err
 		}
 
-		var i64 int64
-		i64, err = strconv.ParseInt(input, 10, 64)
-
-		if err != nil {
-			out := `
-Invalid input, please try again.
-
-Valid integer expressions include: 1, 12, -300 etc.
-		`
-			ui.Output(strings.TrimSpace(out))
-
-		} else {
-			i = int(i64)
-			return
+		i64, err := strconv.ParseInt(input, 10, 64)
+		if err == nil {
+			return int(i64), nil
 		}
+
+		out := "Invalid input, please try again. Valid integer expressions include: 1, 12, -300 etc."
+		ui.Output(strings.TrimSpace(out))
 	}
 }
 
+// timeInput retrieves a time.Time value, but only pays attention
+// to the hour and the minute components. It fills in the year 0,
+// month 0, day 0, second 0 and nsecond 0. It uses time.Local for
+// location information.
+//
+// Use timeInput if you need to retrieve a time of the form 12:45,
+// but if you care also abou the calendrical components, such as
+// the year, month and day, use 'dateInput'.
 func timeInput(ui cli.Ui, text string) (t time.Time, err error) {
-	var hour, min int
-	var useNow bool
 
-	ui.Output(text)
-	useNow, err = yesNo(ui, "You must input a time, would you like to use the current time?")
-	if err != nil {
-		return
+	ui.Output(text + " [time]")
+	if useNow, err := yesNo(ui, "Would you like to use the current time?"); err != nil {
+		return *new(time.Time), err
+	} else if useNow {
+		now := time.Now()
+		return time.Date(0, 0, 0, now.Hour(), now.Minute(), 0, 0, time.Local), nil
 	}
 
-	if useNow {
-		t = time.Now()
-		return
+	var (
+		inputErr  error
+		hour, min int
+	)
+
+	if hour, inputErr = intInput(ui, "Hour [e.g., 13]"); inputErr != nil {
+		return *new(time.Time), inputErr
 	}
 
-	hour, err = intInput(ui, "Hour [e.g., 13]")
-	if err != nil {
-		return
-	}
-	min, err = intInput(ui, "Minute [e.g., 59]")
-	if err != nil {
-		return
+	if min, inputErr = intInput(ui, "Minute [e.g., 59]"); inputErr != nil {
+		return *new(time.Time), inputErr
 	}
 
-	t, err = time.Date(0, 0, 0, hour, min, 0, 0, time.Local), nil
-	return
+	return time.Date(0, 0, 0, hour, min, 0, 0, time.Local), nil
 }
 
-func dateInput(ui cli.Ui, text string) (t time.Time, err error) {
-	var year, month, day, hour, min int
-	var useNow bool
-
-	ui.Output(text)
-	ui.Output("You must input a datetime...")
-	useNow, err = yesNo(ui, "Would you like to use the current time?")
-	if err != nil {
-		return
+// dateInput retrieves a time.Time value as textual input over a
+// a series of messages
+//
+// Use dateInput when you need a full date and time, i.e., 1/1/1 12:00
+// If you only need a time, use 'timeInput'.
+func dateInput(ui cli.Ui, text string) (time.Time, error) {
+	ui.Output(text + " [date]")
+	if useNow, err := yesNo(ui, "Would you like to use the current time?"); err != nil {
+		return *new(time.Time), err
+	} else if useNow {
+		return time.Now(), nil
 	}
 
-	if useNow {
-		t = time.Now()
-		return
+	var (
+		inputErr                    error
+		year, month, day, hour, min int
+	)
+
+	if year, inputErr = intInput(ui, "Year (e.g., 2016)"); inputErr != nil {
+		return *new(time.Time), inputErr
 	}
 
-	year, err = intInput(ui, "Year [e.g., 2015]:")
-	if err != nil {
-		return
-	}
-	month, err = intInput(ui, "Month [e.g., 1 for January")
-	if err != nil {
-		return
-	}
-	day, err = intInput(ui, "Day [e.g., 1]")
-	if err != nil {
-		return
-	}
-	hour, err = intInput(ui, "Hour [e.g., 13]")
-	if err != nil {
-		return
-	}
-	min, err = intInput(ui, "Minute [e.g., 59]")
-	if err != nil {
-		return
+	if month, inputErr = intInput(ui, "Month (e.g., 1 for January)"); inputErr != nil {
+		return *new(time.Time), inputErr
 	}
 
-	t, err = time.Date(year, time.Month(month), day, hour, min, 0, 0, time.Local), nil
-	return
+	if day, inputErr = intInput(ui, "Day [e.g., 1]"); inputErr != nil {
+		return *new(time.Time), inputErr
+	}
+
+	if hour, inputErr = intInput(ui, "Hour [e.g., 13]"); inputErr != nil {
+		return *new(time.Time), inputErr
+	}
+
+	if min, inputErr = intInput(ui, "Minute [e.g., 59]"); inputErr != nil {
+		return *new(time.Time), inputErr
+	}
+
+	return time.Date(year, time.Month(month), day, hour, min, 0, 0, time.Local), nil
 }
